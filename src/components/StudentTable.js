1471bd585599e7fcd9291c312027a6582dd691f4
Ruchira Dilshan Wijerathna <139690978+RuchiraDilshan@users.noreply.github.com>
2024-11-19 09:30:22 +0000

    Create_table

---
+++ src/components/StudentTable.js
@@ -0,0 +1,13 @@
+export default function StudentTable(){
+
+    return(
+        <>
+        <table>
+            <tr>
+                <td></td>
+                </tr>
+
+            </table>
+        </>    
+    )
+}
